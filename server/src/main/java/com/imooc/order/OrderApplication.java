ac65a377b75524de1f28c5eb0c97081f07aea49c
ligen <ligen3@yusys.com.cn>
2020-12-21 22:15:08 +0800

    10-2 触发降级

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -2,12 +2,17 @@ package com.imooc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+
 @EnableFeignClients(basePackages = "com.imooc.product.client")
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+@SpringCloudApplication
 public class OrderApplication {
 
     public static void main(String[] args) {
