82a98862b9e11d321c25a618402d65b39cf7c68c
ligen <ligen3@yusys.com.cn>
2020-12-22 21:09:08 +0800

    10-3 超时设置

--- server/src/main/java/com/imooc/order/controller/HystrixController.java
+++ server/src/main/java/com/imooc/order/controller/HystrixController.java
@@ -2,6 +2,7 @@ package com.imooc.order.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +10,35 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
+/**
+ * Created by 廖师兄
+ * 2018-03-18 20:23
+ */
 @RestController
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
-    @HystrixCommand(fallbackMethod = "fallback")
+
+    //超时配置
+	@HystrixCommand(commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+	})
+//    @HystrixCommand
     @GetMapping("/getProductInfoList")
-    public String getProductInfoList() {
+    public String getProductInfoList(@RequestParam("number") Integer number) {
+
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject("http://127.0.0.1:9081/product/listForOrder",
                 Arrays.asList("157875196366160022"),
                 String.class);
-}
+
+//		throw new RuntimeException("发送异常了");
+    }
 
     private String fallback() {
         return "太拥挤了, 请稍后再试~~";
+    }
+
+    private String defaultFallback() {
+        return "默认提示：太拥挤了, 请稍后再试~~";
     }
 }
